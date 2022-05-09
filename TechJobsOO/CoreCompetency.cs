ef04cb6bd0d7ed05063a0b4b404325d84fc0ea46
easytigers <easytigers176@gmail.com>
2022-05-09 07:41:26 -0400

    first commit

--- TechJobsOO/CoreCompetency.cs
+++ TechJobsOO/CoreCompetency.cs
@@ -3,38 +3,40 @@ namespace TechJobsOO
 {
     public class CoreCompetency
     {
-        private int id;
+        // private int id;
         private static int nextId = 1;
-        private string value;
+        // private string value;
 
         // TODO: Change the fields to auto-implemented properties.
-
+        public int Id { get; }
+        public string Value { get; set; }
+        
         public CoreCompetency()
         {
-            id = nextId;
+            Id = nextId;
             nextId++;
         }
 
         public CoreCompetency(string v) : this()
         {
-            value = v;
+            Value = v;
         }
 
         public override bool Equals(object obj)
         {
             return obj is CoreCompetency competency &&
-                   id == competency.id;
+                   Id == competency.Id;
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(id);
+            return HashCode.Combine(Id);
         }
 
         public override string ToString()
         {
-            return value;
+            return Value;
         }
     }
-    }
+    
 }
